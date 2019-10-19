0400643cf7a5831d48d2a82228ccaf567c13e817
rorywalsh <rorywalsh@ear.ie>
2019-10-19 22:17:49 +0100

    adding msbuild path to windows CI script

--- buildWin64.ps1
+++ buildWin64.ps1
@@ -1,3 +1,6 @@
+$env:Path += ";C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin"
+
+
 dir  
 cd c:/
 Invoke-WebRequest -Uri "https://github.com/rorywalsh/cabbage/releases/download/v2.0.00/csound-windows_x64-6.13.0.zip" -OutFile "C:\csound-windows_x64-6.12.0.zip" 
