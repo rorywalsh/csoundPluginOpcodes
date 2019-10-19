22f48999e93e05c1b5b1313b9e544ba33cb9ee09
rorywalsh <rorywalsh@ear.ie>
2019-10-19 22:23:03 +0100

    adding release build config to window powershell script

--- buildWin64.ps1
+++ buildWin64.ps1
@@ -13,7 +13,7 @@ cd D:/a/1/s/
 mkdir build
 cd build
 cmake -G "Visual Studio 15 2017 Win64" ..
-msbuild.exe Project.sln /property:Platform=x64
+msbuild.exe Project.sln /property:Platform=x64 /property:Configuration=Release
 
 Copy-Item "D:/a/1/s/build/Release/*" -Destination D:/a/1/a/*
 
